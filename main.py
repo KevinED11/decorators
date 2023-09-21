dd04fd5ca6b03e3ae94c01c4cdd9b6a7020b6bfc
KevinED11 <mendez_kael@hotmail.com>
2023-09-21 07:48:29 -0700

    add class decorator

--- main.py
+++ main.py
@@ -12,8 +12,24 @@ def say_hello():
     print("hello")
 
 
+class MyDecorator:
+    def __init__(self, func: callable):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        print("before")
+        self.func(*args, **kwargs)
+        print("after")
+
+
+@MyDecorator
+def say_hello2():
+    print("hello2")
+
+
 def main():
     say_hello()
+    say_hello2()
 
 
 if __name__ == '__main__':
